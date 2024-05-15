8446ca7f1273d0995035dacdcb3b8e80ecfece0a
Ida Hellqvist <ida.s.hellqvist@gmail.com>
2024-05-15 12:22:52 +0200

    Extract output management + testing to separate modules

--- output_management.py
+++ output_management.py
@@ -1,11 +1,16 @@
 import datetime
+from os import makedirs
+from shutil import copyfile
+
 from configuration_setup import Configuration
-import os  # TODO: Is the entire os module needed?
 
 
 def set_up_output_directory(config: Configuration):
     """
-    Generates up the output directory for the generated spectra.
+    Create the output directory and a subdirectory for temp files
+
+    Args:
+        config (Configuration): Configuration object contining the path to the output directory to be created
     """
     # Get current date and time as YYYYMMDD_HH:MM
     now = datetime.now().strftime("%Y-%m-%d@%H:%M")
@@ -13,6 +18,52 @@ def set_up_output_directory(config: Configuration):
     # Create the path to the output directory
     config.path_output_directory = f"{config.path_output_directory}/{now}"
 
-    # Create the directory
-    os.makedirs(config.path_output_directory)
-    os.makedirs(f"{config.path_output_directory}/opac")
+    # Create the directory + subdirectory for temp files
+    makedirs(config.path_output_directory)
+    makedirs(f"{config.path_output_directory}/temp")
+
+
+def copy_config_file(config: Configuration):
+    """
+    Copy the configuration file to the output directory
+
+    Args:
+        config (Configuration): Configuration object containing the path to the configuration file and the output directory
+    """
+    copyfile(config.path_config, config.path_output_directory)
+
+
+def generate_parameter_file(
+    config: Configuration,
+    successful_parameters: list,
+    no_files_found_for_interpolation: list,
+    multiple_files_found_for_interpolation: list,
+):
+    # Create a file in the output directory
+    with open(f"{config.path_output_directory}/stellar_parameters.txt", "w") as file:
+
+        if no_files_found_for_interpolation:
+            file.write("----------------------------------------\n")
+            file.write("No spectrum generated because files\n")
+            file.write("needed for interpolation were not found:\n")
+            file.write("----------------------------------------\n")
+
+            for parameter_set in no_files_found_for_interpolation:
+                file.write(f"{parameter_set}\n")
+
+        if multiple_files_found_for_interpolation:
+            file.write("\n\n----------------------------------------\n")
+            file.write("No spectrum generated because multiple\n")
+            file.write("matching model atmospheres were found\n")
+            file.write("for interpolation:\n")
+            file.write("----------------------------------------\n")
+
+            for parameter_set in multiple_files_found_for_interpolation:
+                file.write(f"{parameter_set}\n")
+
+        file.write("\n\n----------------------------------------\n")
+        file.write("Spectra generated:\n")
+        file.write("----------------------------------------\n")
+        # Write successful parameters
+        for parameter_set in successful_parameters:
+            file.write(f"{parameter_set}\n")
--- tests/test_output_management.py
+++ tests/test_output_management.py
@@ -1,8 +1,181 @@
 import unittest
-from unittest.mock import patch, MagicMock
-import subprocess
+from unittest.mock import MagicMock, mock_open, patch
+
+import output_management
 from configuration_setup import Configuration
-import turbospectrum_integration
-import parameter_generation
-import os
-from shutil import rmtree
+
+
+class TestOutputManagement(unittest.TestCase):
+
+    @patch("output_management.makedirs")
+    @patch("output_management.datetime")
+    def test_set_up_output_directory(self, mock_datetime, mock_makedirs):
+        """
+        Test that the output directory is created with the correct name and a subdirectory for temp files
+        """
+        # Mock the current date and time
+        mock_now = mock_datetime.now.return_value
+        mock_now.strftime.return_value = "2024-05-04@12:00"
+
+        # Create a mock Configuration object
+        config = MagicMock(spec=Configuration)
+        config.path_output_directory = "/path/to/output"
+
+        # Call the function
+        output_management.set_up_output_directory(config)
+
+        # Versify that the paths are generated correctly
+        expected_output_directory = "/path/to/output/2024-05-04@12:00"
+        expected_temp_directory = "/path/to/output/2024-05-04@12:00/temp"
+
+        self.assertEqual(config.path_output_directory, expected_output_directory)
+
+        # Verify that os.makedirs was called with the correct arguments
+        mock_makedirs.assert_any_call(expected_output_directory)
+        mock_makedirs.assert_any_call(expected_temp_directory)
+
+    @patch("output_management.copyfile")
+    def test_copy_config_file(self, mock_copyfile):
+        """
+        Test that the configuration file is copied to the output directory
+        """
+        # Create a mock Configuration object
+        config = MagicMock(spec=Configuration)
+        config.path_config = "/path/to/config"
+        config.path_output_directory = "/path/to/output"
+
+        # Call the function
+        output_management.copy_config_file(config)
+
+        # Verify that os.copyfile was called with the correct arguments
+        mock_copyfile.assert_called_once_with("/path/to/config", "/path/to/output")
+
+    @patch("output_management.open", new_callable=mock_open)
+    def test_generate_parameter_file(self, mock_open):
+        """
+        Test that the parameter file is generated correctly
+        """
+        # Create a mock Configuration object
+        config = MagicMock(spec=Configuration)
+        config.path_output_directory = "/path/to/output"
+
+        # Define parameter sets
+        successful_parameters = [
+            {"Teff": 5000, "logg": 4.5, "FeH": 0.0},
+            {"Teff": 6000, "logg": 4.0, "FeH": -0.5},
+        ]
+        no_files_found_for_interpolation = [
+            {"Teff": 7000, "logg": 4.5, "FeH": 0.0},
+            {"Teff": 8000, "logg": 4.0, "FeH": -0.5},
+        ]
+        multiple_files_found_for_interpolation = [
+            {"Teff": 9000, "logg": 4.5, "FeH": 0.0},
+            {"Teff": 10000, "logg": 4.0, "FeH": -0.5},
+        ]
+
+        # Call the function
+        output_management.generate_parameter_file(
+            config,
+            successful_parameters,
+            no_files_found_for_interpolation,
+            multiple_files_found_for_interpolation,
+        )
+
+        # Get file handle from mock
+        file_handle = mock_open.return_value.__enter__.return_value
+
+        # Define expexted file content
+        expected_content = (
+            "----------------------------------------\n"
+            "No spectrum generated because files\n"
+            "needed for interpolation were not found:\n"
+            "----------------------------------------\n"
+            f"{no_files_found_for_interpolation[0]}\n"
+            f"{no_files_found_for_interpolation[1]}\n"
+            "\n\n----------------------------------------\n"
+            "No spectrum generated because multiple\n"
+            "matching model atmospheres were found\n"
+            "for interpolation:\n"
+            "----------------------------------------\n"
+            f"{multiple_files_found_for_interpolation[0]}\n"
+            f"{multiple_files_found_for_interpolation[1]}\n"
+            "\n\n----------------------------------------\n"
+            "Spectra generated:\n"
+            "----------------------------------------\n"
+            f"{successful_parameters[0]}\n"
+            f"{successful_parameters[1]}\n"
+        )
+
+        # Verify that the file was written with the correct content
+        file_handle.write.assert_any_call("----------------------------------------\n")
+        file_handle.write.assert_any_call("No spectrum generated because files\n")
+        file_handle.write.assert_any_call("needed for interpolation were not found:\n")
+        file_handle.write.assert_any_call("----------------------------------------\n")
+        file_handle.write.assert_any_call(f"{no_files_found_for_interpolation[0]}\n")
+        file_handle.write.assert_any_call(f"{no_files_found_for_interpolation[1]}\n")
+        file_handle.write.assert_any_call(
+            "\n\n----------------------------------------\n"
+        )
+        file_handle.write.assert_any_call("No spectrum generated because multiple\n")
+        file_handle.write.assert_any_call("matching model atmospheres were found\n")
+        file_handle.write.assert_any_call("for interpolation:\n")
+        file_handle.write.assert_any_call("----------------------------------------\n")
+        file_handle.write.assert_any_call(
+            f"{multiple_files_found_for_interpolation[0]}\n"
+        )
+        file_handle.write.assert_any_call(
+            f"{multiple_files_found_for_interpolation[1]}\n"
+        )
+        file_handle.write.assert_any_call(
+            "\n\n----------------------------------------\n"
+        )
+        file_handle.write.assert_any_call("Spectra generated:\n")
+        file_handle.write.assert_any_call(f"{successful_parameters[0]}\n")
+        file_handle.write.assert_any_call(f"{successful_parameters[1]}\n")
+
+    @patch("output_management.open", new_callable=mock_open)
+    def test_generate_parameter_file_only_successful_parameters(self, mock_open):
+        """
+        Test that the parameter file is generated correctly when
+        there were only successfull parameters passed
+        """
+        # Create a mock Configuration object
+        config = MagicMock(spec=Configuration)
+        config.path_output_directory = "/path/to/output"
+
+        # Define parameter sets
+        successful_parameters = [
+            {"Teff": 5000, "logg": 4.5, "FeH": 0.0},
+            {"Teff": 6000, "logg": 4.0, "FeH": -0.5},
+        ]
+        no_files_found_for_interpolation = []
+        multiple_files_found_for_interpolation = []
+
+        # Call the function
+        output_management.generate_parameter_file(
+            config,
+            successful_parameters,
+            no_files_found_for_interpolation,
+            multiple_files_found_for_interpolation,
+        )
+
+        # Get file handle from mock
+        file_handle = mock_open.return_value.__enter__.return_value
+
+        # Define expexted file content
+        expected_content = (
+            "\n\n----------------------------------------\n"
+            "Spectra generated:\n"
+            "----------------------------------------\n"
+            f"{successful_parameters[0]}\n"
+            f"{successful_parameters[1]}\n"
+        )
+
+        # Verify that the file was written with the correct content
+        file_handle.write.assert_any_call(
+            "\n\n----------------------------------------\n"
+        )
+        file_handle.write.assert_any_call("Spectra generated:\n")
+        file_handle.write.assert_any_call("----------------------------------------\n")
+        file_handle.write.assert_any_call(f"{successful_parameters[0]}\n")
+        file_handle.write.assert_any_call(f"{successful_parameters[1]}\n")
